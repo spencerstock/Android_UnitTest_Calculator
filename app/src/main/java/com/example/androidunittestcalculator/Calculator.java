ad1d12fc4b259a26e9fc513735d70c7e21d8afc2
Spencer Stock <spencerstock@gmail.com>
2019-05-08 14:28:45 -0600

    added test for operator

--- app/src/main/java/com/example/androidunittestcalculator/Calculator.java
+++ app/src/main/java/com/example/androidunittestcalculator/Calculator.java
@@ -6,6 +6,7 @@ public class Calculator {
 
     private String  displayString = "";
     private Pattern NUMERICAL     = Pattern.compile("[0-9]");
+    private Pattern OPERANDS      = Pattern.compile("[?:/*+\\-]");
     private float   operand1;
     private float   operand2;
     private char    operator;
@@ -13,18 +14,29 @@ public class Calculator {
     public Calculator() {
     }
 
-    public String addSymbol(String symbol) { //saves string
+    public String addSymbol(String symbol) {
 
         if (symbol.length() != 1) return null;
-        if (NUMERICAL.matcher(symbol).matches()) addNumber(symbol);
+        if (NUMERICAL.matcher(symbol).matches())
+            addNumber(symbol);
 
 
-        if (symbol.equals(".")) {
+        if (symbol.equals("."))
             addDecimal(symbol);
-        }
 
+        if (OPERANDS.matcher(symbol).matches()) addOperator(symbol);
 
         return displayString;
+
+    }
+
+    private void addOperator(String symbol) {
+        if (operator == '\0') {
+            char[] temp = symbol.toCharArray();
+            operator = temp[0];
+            operand1 = Float.valueOf(displayString);
+            displayString = "";
+        }
 
     }
 
@@ -38,5 +50,9 @@ public class Calculator {
         if (displayString.contains(".")) return null;
         displayString += symbol;
         return symbol;
+    }
+
+    public char getOperator() {
+        return operator;
     }
 }
--- app/src/test/java/com/example/androidunittestcalculator/CalculatorUnitTest.java
+++ app/src/test/java/com/example/androidunittestcalculator/CalculatorUnitTest.java
@@ -10,28 +10,39 @@ public class CalculatorUnitTest {
     @Test
     public void addSymbol_ReturnsString() {
         Calculator calculator = new Calculator();
-        assertEquals("a", calculator.addSymbol("a"));
+        assertEquals("5", calculator.addSymbol("5"));
     }
 
     @Test
     public void addMultipleSymbols_ReturnsString() {
         Calculator calculator = new Calculator();
-        assertEquals("a", calculator.addSymbol("a"));
-        assertEquals("ab", calculator.addSymbol("b"));
-        assertEquals("ab.", calculator.addSymbol("."));
+        assertEquals("5", calculator.addSymbol("5"));
+        assertEquals("56", calculator.addSymbol("6"));
+        assertEquals("56.", calculator.addSymbol("."));
     }
 
     @Test
     public void addMultipleDecimals_ReturnsNull() {
         Calculator calculator = new Calculator();
         assertEquals(".", calculator.addSymbol("."));
-        assertNull(calculator.addSymbol("."));
-        assertEquals(".c", calculator.addSymbol("c"));
+        assertEquals(".", calculator.addSymbol("."));
+        assertEquals(".1", calculator.addSymbol("1"));
     }
 
     @Test
     public void addMultipleSymbols_ReturnsNull() {
         Calculator calculator = new Calculator();
-        assertNull(calculator.addSymbol("abc"));
+        assertNull(calculator.addSymbol("123"));
     }
+
+    @Test
+    public void addMinusSymbol_ReturnsString() {
+        Calculator calculator = new Calculator();
+        assertEquals(".", calculator.addSymbol("."));
+        assertEquals(".1", calculator.addSymbol("1"));
+        assertEquals("", calculator.addSymbol("-"));
+        assertEquals('-', calculator.getOperator());
+
+    }
+
 }
