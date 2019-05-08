54e576ee8aea3a2c56647061ddf4bc56cfb73cb1
Spencer Stock <spencerstock@gmail.com>
2019-05-08 14:10:57 -0600

    made calculator methods dynamic

--- app/src/main/java/com/example/androidunittestcalculator/Calculator.java
+++ app/src/main/java/com/example/androidunittestcalculator/Calculator.java
@@ -1,15 +1,42 @@
 package com.example.androidunittestcalculator;
 
+import java.util.regex.Pattern;
+
 public class Calculator {
 
-    private static String savedSymbol;
+    private String  displayString = "";
+    private Pattern NUMERICAL     = Pattern.compile("[0-9]");
+    private float   operand1;
+    private float   operand2;
+    private char    operator;
 
-    public static String addSymbol(String symbol) { //saves string
+    public Calculator() {
+    }
 
-        if (savedSymbol == null) {
-            savedSymbol = symbol;
+    public String addSymbol(String symbol) { //saves string
+
+        if (symbol.length() != 1) return null;
+        if (NUMERICAL.matcher(symbol).matches()) addNumber(symbol);
+
+
+        if (symbol.equals(".")) {
+            addDecimal(symbol);
         }
-        return savedSymbol;
 
+
+        return displayString;
+
+    }
+
+
+    private String addNumber(String symbol) {
+        displayString += symbol;
+        return symbol;
+    }
+
+    private String addDecimal(String symbol) {
+        if (displayString.contains(".")) return null;
+        displayString += symbol;
+        return symbol;
     }
 }
--- app/src/test/java/com/example/androidunittestcalculator/CalculatorUnitTest.java
+++ app/src/test/java/com/example/androidunittestcalculator/CalculatorUnitTest.java
@@ -3,19 +3,35 @@ package com.example.androidunittestcalculator;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class CalculatorUnitTest {
 
     @Test
     public void addSymbol_ReturnsString() {
-        assertEquals("Test", Calculator.addSymbol("Test"));
-
+        Calculator calculator = new Calculator();
+        assertEquals("a", calculator.addSymbol("a"));
     }
 
     @Test
-    public void addSymbol_ReturnsMultipleStrings() {
-        assertEquals("Test2", Calculator.addSymbol("Test2"));
-        assertEquals("Test3", Calculator.addSymbol("Test3"));
+    public void addMultipleSymbols_ReturnsString() {
+        Calculator calculator = new Calculator();
+        assertEquals("a", calculator.addSymbol("a"));
+        assertEquals("ab", calculator.addSymbol("b"));
+        assertEquals("ab.", calculator.addSymbol("."));
+    }
 
+    @Test
+    public void addMultipleDecimals_ReturnsNull() {
+        Calculator calculator = new Calculator();
+        assertEquals(".", calculator.addSymbol("."));
+        assertNull(calculator.addSymbol("."));
+        assertEquals(".c", calculator.addSymbol("c"));
+    }
+
+    @Test
+    public void addMultipleSymbols_ReturnsNull() {
+        Calculator calculator = new Calculator();
+        assertNull(calculator.addSymbol("abc"));
     }
 }
